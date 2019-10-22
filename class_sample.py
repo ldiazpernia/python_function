17eda1d9f245564b8cd267a3fc8c54a99b2cb7b9
Sagan <ldiazpernia@gmail.com>
2019-10-22 03:04:52 -0400

    Level 3 done

---
+++ class_sample.py
@@ -0,0 +1,33 @@
+# class Point:
+#     """ Point class for representing and manipulating x,y coordinates. """
+
+#     def __init__(self, initX, initY):
+
+#         self.x = initX
+#         self.y = initY
+
+#     def getX(self):
+#         return self.x
+
+#     def getY(self):
+#         return self.y
+
+#     def distanceFromOrigin(self):
+#         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+
+# p = Point(7,6)
+# print(p.distanceFromOrigin())
+
+
+
+class Animal:
+    def __init__(self , arm ,leg):
+        self.arms = arm
+        self.legs = leg
+        
+    def limbs (self):
+        return (self.arms + self.legs)
+spider  = Animal (4,4)       
+spidlimbs = spider.limbs()
+print (spidlimbs)
